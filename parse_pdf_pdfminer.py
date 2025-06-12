6d802989a2d9b8c075c56e5fc9ee4b0d83f7cf74
Peeyush4 <peeyu704@gmail.com>
2025-06-12 17:21:41 -0400

    Trimmed

--- parse_pdf_pdfminer.py
+++ parse_pdf_pdfminer.py
@@ -19,7 +19,14 @@ interpreter = PDFPageInterpreter(rsrc_mgr, device)
 # Open the PDF file
 with open(pdf_path, "rb") as fp:
     # Process each page in the PDF
+
+    # The first 18 pages are not needed, so we will skip them
+    # References are also not needed. So we will skip these as well
+    page_number = 0
     for page in tqdm(PDFPage.get_pages(fp)):
+        page_number += 1
+        if page_number <= 18 or page_number > 644:
+            continue
         interpreter.process_page(page)
     # Get the text from the StringIO object
     text = retstr.getvalue()
@@ -28,8 +35,7 @@ device.close()
 retstr.close()
 
 # Print or save the extracted text
-# print(text)
-
+text = text.replace("\n\nAccess for free at openstax.org", "") 
 # Optionally, save the text to a file
-with open("Data/Psychology2e_WEB_pdfminer.txt", "w", encoding="utf-8") as text_file:
+with open("Data/Psychology2e_WEB_pdfminer_trimmed.txt", "w", encoding="utf-8") as text_file:
     text_file.write(text)
